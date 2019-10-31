695b5fe2dc8c35b14bd3ff4b017237235b16ac7c
kouamano <amano.au1@gmail.com>
2019-10-31 17:44:52 +0900

    add

---
+++ MPI/small-test/mpitest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+int main(int argc, char **argv){
+        int my_rank;
+        int p;
+        int source;
+        int dest;
+        int tag = 0;
+        char message[100];
+
+        printf("prog:mpi-test:\n");
+
+        MPI_Status status;
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+        printf("rank:%d:\n",my_rank);
+        printf("p:%d:\n",p);
+
+        MPI_Finalize();
+
+}
+
