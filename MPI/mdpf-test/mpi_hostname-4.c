a6791501afd376ea2e75aba9f2be322d9350ce4d
kouamano <amano.au1@gmail.com>
2019-12-05 15:44:32 +0900

    add

--- MPI/mdpf-test/mpi_hostname-4.c
+++ MPI/mdpf-test/mpi_hostname-4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <mpi.h>
 
@@ -11,7 +12,7 @@ int main(int argc, char **argv){
 	int i = 0;
         char message[100];
 
-        printf("prog:mpi-test:\n");
+        //printf("prog:mpi-test:\n");
 
         MPI_Status status;
         MPI_Init(&argc, &argv);
@@ -21,8 +22,10 @@ int main(int argc, char **argv){
 	for(i=0;i<100;i++){
 		i + 1;
 	}
+	char hostname[128];
+	gethostname(hostname, sizeof(hostname));
 
-        printf("rank:%d/%d:\n",my_rank,p);
+        printf("rank:%d/%d:@%s\n",my_rank,p,hostname);
         //printf("p:%d:\n",p);
 
         MPI_Finalize();
